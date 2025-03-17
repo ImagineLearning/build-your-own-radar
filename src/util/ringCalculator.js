bcc6204ed2addebd17b177cde0bad546567aeff6
Paul Palladino <paul.palladino@imaginelearning.com>
2025-03-17 11:08:04 -0400

    feat: 1) updated styles of rings such that color consistantly gets lighter from the inner most to the outer most ring 2) increased the thickness of the new rings to accomidate adding pogs and retain visual correctness

--- src/util/ringCalculator.js
+++ src/util/ringCalculator.js
@@ -1,6 +1,7 @@
 const RingCalculator = function (numberOfRings, maxRadius) {
   // This sequence is used to calculate the radius of each ring
-  var sequence = [0, 6, 5, 3, 2, 1, 1, 1, 1, 1]
+  // Higher values create thicker rings
+  var sequence = [0, 6, 5, 3, 2, 2, 2, 1, 1, 1]
 
   var self = {}
 
